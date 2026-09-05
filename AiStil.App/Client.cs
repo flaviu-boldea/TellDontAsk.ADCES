f0a3628edd5fe6b5372bf3ad77d538270e300031
flaviu-boldea <boldicu@yahoo.co.uk>
2026-09-05 13:55:51 +0000

    Align pricing with stylist base cost and client discount

--- AiStil.App/Client.cs
+++ AiStil.App/Client.cs
@@ -6,20 +6,15 @@ public class Client
     public int ClientId { get; set; }
     public required string Membership { get; set; }
 
-    public decimal CalculateCost(decimal baseCost)
+    private decimal CalculateCost(decimal baseCost)
     {
         return Membership == "Standard" ? baseCost : 0;
-    }
-
-    public decimal PriceFor(Service service)
-    {
-        return CalculateCost(service.Price);
     }
 
     public Appointment BookAppointment(Stylist stylist, Service service, Slot slot)
     {
         Appointment appointment = stylist.MakeAppointment(slot, service);
-        appointment.Cost = PriceFor(service);
+        appointment.Cost = CalculateCost(appointment.Cost);
         appointment.ClientId = ClientId;
         return appointment;
     }
